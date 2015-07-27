96ce723e3e4d994dfd276f93b0dfb0866a78fabb
rdpse <rduartenes@gmail.com>
2015-07-27 17:17:55 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
+HOMEDIR=$(echo $HOME)
 PJS_VERSION=1.9.8
-declare -a deps=('ruby' 'screen' 'git')
 
-if [ -f /etc/debian_version ] || [ -f /etc/lsb-release ]; then
-    apt-get -y install ${deps[@]}
-elif [ -f /etc/redhat-release ]; then
-    yum -y install ${deps[@]}
+if [ -d $HOMEDIR/kimiao ]; then
+  export PATH=$HOMEDIR/phantomjs/bin:$PATH
+  screen -S kimiao sh -c 'ruby '$HOMEDIR'/kimiao/kimiao.rb'
+  exit 0
 fi
 
-cd ~
+declare -a deps=('ruby' 'ruby-dev' 'screen' 'git' 'build-essential' 'bison' 'openssl'
+  'libreadline6' 'libreadline6-dev' 'curl' 'git-core' 'zlib1g' 'zlib1g-dev'
+  'libssl-dev' 'libyaml-dev' 'libxml2-dev' 'autoconf' 'libc6-dev' 'ncurses-dev'
+  'automake' 'libtool' 'libfontconfig1-dev')
+
+declare -a depsrhel=('ruby' 'ruby-devel' 'screen' 'git' 'gcc-c++' 'kernel-devel' 'bison' 'openssl-devel'
+  'readline-devel' 'curl' 'git-core' 'zlib-devel'
+  'libyaml-devel' 'libxml2-devel' 'autoconf' 'glibc-devel' 'ncurses-devel'
+  'automake' 'libtool' 'libfontconfig1-dev')
+
+if [ -f /etc/debian_version ] || [ -f /etc/lsb-release ]; then
+    sudo apt-get -y install ${deps[@]}
+elif [ -f /etc/redhat-release ]; then
+    sudo yum -y install ${depsrhel[@]}
+fi
+
+cd $HOMEDIR
 wget -O- https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-$PJS_VERSION-linux-x86_64.tar.bz2 | tar xj
 mv phantomjs-$PJS_VERSION-linux-x86_64 phantomjs
-ln -s phantomjs/bin/phantomjs /usr/local/sbin/phantomjs
+export PATH=$HOMEDIR/phantomjs/bin:$PATH
 
-git clone https://github.com/rdpse/kimiao.git .
-screen -S kimiao ./kimiao.rb
-
-
-
-
-
+git clone https://github.com/rdpse/kimiao.git
+cd $HOMEDIR/kimiao/
+  sudo gem install capybara poltergeist
+  chmod +x kimiao.rb
+  screen -S kimiao sh -c 'ruby kimiao.rb'
