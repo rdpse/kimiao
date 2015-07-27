11d979e13878e03cac03df1d8c1087b876fd97c1
Rúben Enes <rduartenes@gmail.com>
2015-07-27 07:37:22 +0100

    n2

---
+++ kimiao.rb
@@ -0,0 +1,113 @@
+#!/usr/bin/env ruby
+#encoding: utf-8
+
+require 'bundler/setup'
+
+require 'capybara'
+require 'capybara/dsl'
+require 'capybara/poltergeist'
+require 'io/console'
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, :window_size => [1024, 768])
+end
+
+Capybara.javascript_driver = :poltergeist
+Capybara.current_driver = :poltergeist
+Capybara.app_host = 'http://kimsufi.com/fr'
+Capybara.default_wait_time = 10
+
+$oproof = 'order.png'
+
+if File.exist?($oproof)
+    puts 'Server has been acquired already.'
+    exit(-1)
+end
+
+$svmodels = { "KS-1"           =>   "150sk10",
+                    "KS-2"           =>   "150sk20",
+                    "KS-2 SSD"   =>   "150sk22",
+                    "KS-3"            =>   "150sk30",
+                    "KS-4"            =>   "150sk40",
+                    "KS-5"            =>   "150sk50",
+                    "KS-6"            =>   "150sk60",
+                  }
+
+puts 'Specify the server model (ex. KS-1, KS-2 SSD, etc):'
+STDOUT.flush
+$svm = nil
+until  $svmodels.has_key?($svm)
+    $svm = gets.chomp.upcase
+    unless $svmodels.has_key?($svm)
+        puts 'Please enter a valid server model:'
+    end
+end
+
+puts 'Enter quantity [1-5]:'
+STDOUT.flush
+$svq = nil
+until $svq.is_a?(Fixnum) && $svq.between?(1, 5)
+  $svq = Integer(gets.chomp) rescue nil
+  unless $svq.is_a?(Fixnum) && $svq.between?(1, 5)
+     puts 'Please enter an integer between 1 and 5:'
+  end
+end
+
+#def choices (var, methods*)
+#    if methods > 1
+#       until var.method[1] && var.method[2]
+#
+
+puts 'Enter your your KS account email:'
+STDOUT.flush
+$ovhemail = gets.chomp
+
+puts 'Enter your your KS account password:'
+STDOUT.flush
+$ovhpass = STDIN.noecho(&:gets).chomp
+
+system "clear"
+puts 'Opening browser and checking availability...'
+
+class Order
+  include Capybara::DSL
+
+  def do_order
+    opage = "/commande/kimsufi.xml?reference=#{$svmodels[$svm]}&quantity=#{$svq}"
+    avail =
+
+    until avail
+      begin
+        visit opage
+        sleep(2)
+        avail = page.has_text?('Récapitulatif de votre commande')
+        time = Time.new.strftime("%H:%M:%S")
+        if avail
+          page.execute_script("document.getElementById('customerType-existing').click()")
+          page.fill_in 'Email', :with => "#{$ovhemail}"
+          page.fill_in 'Mot de passe', :with => "#{$ovhpass}"
+          page.click_button('S\'identifier')
+
+          page.check('contracts-validation')
+          page.check('customConractAccepted')
+
+          page.click_button('Confirmer et payer ma commande')
+          sleep(5)
+
+          page.save_screenshot($oproof)
+          puts "[#{time}] Your #{$svm} server has been successfully acquired."
+          exit(-1)
+        end
+      rescue Capybara::Poltergeist::TimeoutError
+        retry
+      rescue Capybara::Poltergeist::StatusFailError
+	retry
+      else
+        puts "[#{time}] Server is not available yet."
+      end
+    end
+  end
+end
+
+Order.new.do_order
+
---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PJS_VERSION=1.9.8
+declare -a deps=('ruby' 'screen' 'git')
+
+if [ -f /etc/debian_version ] || [ -f /etc/lsb-release ]; then
+    apt-get -y install ${deps[@]}
+elif [ -f /etc/redhat-release ]; then
+    yum -y install ${deps[@]}
+fi
+
+cd ~
+wget -O- https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-$PJS_VERSION-linux-x86_64.tar.bz2 | tar xj
+mv phantomjs-$PJS_VERSION-linux-x86_64 phantomjs
+ln -s phantomjs/bin/phantomjs /usr/local/sbin/phantomjs
+
+git clone <rep>
+screen -S kimiao ./kimiao.rb
+
+
+
+
+
