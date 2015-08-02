02ac22a7691265345da75bab97de71fadfffb240
rdpse <rduartenes@gmail.com>
2015-08-02 11:26:26 +0100

    Update kimiao.rb

--- kimiao.rb
+++ kimiao.rb
@@ -16,20 +16,43 @@ end
 
 Capybara.javascript_driver = :poltergeist
 Capybara.current_driver = :poltergeist
-Capybara.app_host = 'http://kimsufi.com/fr'
 Capybara.default_wait_time = 10
 
 $svmodels = { 
-	"KS-1"           =>   "150sk10",
-	"KS-2"           =>   "150sk20",
-	"KS-2 SSD"       =>   "150sk22",
-	"KS-3"           =>   "150sk30",
-	"KS-4"           =>   "150sk40",
-	"KS-5"           =>   "150sk50",
-	"KS-6"           =>   "150sk60"
+	"KS-1"          =>    "150sk10",
+	"KS-2"          =>    "150sk20",
+	"KS-2 SSD"      =>    "150sk22",
+	"KS-3"          =>    "150sk30",
+	"KS-4"          =>    "150sk40",
+	"KS-5"          =>    "150sk50",
+	"KS-6"          =>    "150sk60",
+	
+	"SYS-IP-1"    	=>    "142sys4",
+	"SYS-IP-2"    	=>    "142sys5",
+	"SYS-IP-4"    	=>    "142sys8",
+  "SYS-IP-5"		  =>    "142sys6",	
+  "SYS-IP-5S"	  	=>		"142sys10",    	
+	"SYS-IP-6"			=>		"142sys7",
+	"SYS-IP-6S"			=>		"142sys9",
+	
+	"E3-SSD-1"			=>		"143sys13",
+	"E3-SSD-2"			=>		"143sys10",
+	"E3-SSD-3"			=>		"143sys11",
+	"E3-SSD-4"			=>		"143sys12",
+	
+	"E3-SAT-1"			=>		"143sys4",
+	"E3-SAT-2"			=>		"143sys1",
+	"E3-SAT-3"			=>		"143sys2",
+	"E3-SAT-4"			=>		"143sys3"
+	
+	"BK-8T"					=>		"141bk1",   
+  "BK-24T"				=>		"141bk2",   
+  
+  "GAME-1"    		=>		"141game1",
+  "GAME-2"				=>	  "141game2"
 }
 
-puts 'Specify the server model (ex. KS-1, KS-2 SSD, etc):'
+puts 'Specify the server model (ex. KS-1, KS-2 SSD,  etc):'
 STDOUT.flush
 $svm = nil
 until  $svmodels.has_key?($svm)
@@ -69,8 +92,11 @@ class Order
   include Capybara::DSL
 
   def crawl_and_order
-    kspage = "/commande/kimsufi.xml?reference=#{$svmodels[$svm]}&quantity=#{$svq}"
-    syspage =
+  	if /sk/.match($svq)
+    	kspage = "https://kimsufi.com/fr/commande/kimsufi.xml?reference=#{$svmodels[$svm]}&quantity=#{$svq}"
+    else
+    	syspage = "https://eu.soyoustart.com/fr/commande/soYouStart.xml?reference=#{$svmodels[$svm]}&quantity=#{$svq}"
+    
     avail =
 
     until avail
